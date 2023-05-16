21df280ce1bf65145d828b94f1b960ea1646ed43
madhumitha-rathinasamy <madhumitha@sparkouttech.com>
2023-05-16 13:30:48 +0530

    Swithcase and hidden in html file

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,23 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: `
-  <h2>{{ title }}</h2>
-  <ul>
-    <li *ngIf="courses.length > 0; then listCourses else NoCourses"></li>
-  </ul>
-  <ng-template #listCourses>
-  <li *ngFor="let course of courses">
-      {{ course }}
-    </li>
-</ng-template>
-
-<ng-template #NoCourses>
-    No courses
-</ng-template>
-  `
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
-  title = "List of courses";
-  courses = ["course1", "course2", "course3"];
+  // title = "List of courses";
+  // courses = ["course1", "course2"];
+  viewMode = 'non';
 }
